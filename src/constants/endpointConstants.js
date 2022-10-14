9521f3ce408e6214f6c7e59f7a56d629a1782331
SaraLopez03 <93801979+SaraLopez03@users.noreply.github.com>
2022-10-13 22:29:16 -0500

    [MLGROUP-123]Login Page (#1)
    * login finished
    
    * correction title

---
+++ src/constants/endpointConstants.js
@@ -0,0 +1,6 @@
+
+export const ENDPOINT = {
+    POST_LOGIN: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/user/login",
+    POST_NEW_PRODUCT: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/product/product",
+    GET_PRODUCTS: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/product/product",
+}
