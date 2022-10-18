5050e711f81457e79dc92bd59f9e866c67368c76
Sara Lopez <sara-lohe@hotmail.com>
2022-10-17 23:41:21 -0500

    complete nav bar with all the requirements

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,44 +1,60 @@
 import React from "react";
 import logo from "../../assets/Logo-pri-licores.png";
+import {useNavigate,useLocation, NavLink} from "react-router-dom";
 
 
-const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-faded style-nav">
-            <img className="navbar-brand style-nav-logo" src= {logo}/>
-            <div className="collapse navbar-collapse justify-content-end" id="nav-content">   
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link style-nav-link" href="#">CAJA</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link style-nav-link" href="#">INVENTARIO</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link style-nav-link" href="#">TOTAL VENTAS</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link style-nav-link" href="#">CIERRE CAJA</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        {/* <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Menu</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#caja">Caja</a>
-                            <a class="dropdown-item" href="#inventario">Inventario</a>
-                            <a class="dropdown-item" href="#total">Total Ventas</a>
-                            <a class="dropdown-item" href="#cierre">Cierre Caja</a>
-                        </div> */}
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link logout" >
-                            <i className="fa-solid fa-right-from-bracket"></i>
+const NavBar = ({name}) => {
+    const navigate = useNavigate ();
+
+    let location = useLocation();
+
+    const Logout = () => {
+        localStorage.removeItem("userToken")
+        navigate("")
+    }
+
+    const showNavBar = () => {
+        if(location.pathname === "/"){
+            return ""
+        } else {
+            return (
+                <nav className="navbar navbar-expand-lg style-nav">
+                    <div className="container-fluid">
+                        <div className="d-flex align-items-center">
+                            <img className="navbar-brand style-nav-logo" src= {logo}/>
+                            <p className="style-name"> {name} </p>
+                        </div>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                    </li>
-                </ul>
-              
-            </div>
-        </nav>
-    )
+                        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">   
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <NavLink className= "nav-link style-nav-link" to="/caja" >CAJA </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link style-nav-link" to="/inventario" >INVENTARIO </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link style-nav-link" to="/total-ventas" >TOTAL VENTAS </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link style-nav-link" to="/cierre-caja" >CIERRE CAJA </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="nav-link logout style-nav-link" >
+                                        <i className="fa-solid fa-right-from-bracket" onClick={Logout}></i>
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            )
+        }
+    }
+      
+    return showNavBar();
 }
 
 export default NavBar;
---
+++ src/pages/ClosedCashier/ClosedCashierPage.js
@@ -0,0 +1,10 @@
+const ClosedCashier = () => {
+    return (
+        <div>
+            <h1> HOLA SOY LA PAGINA  CLOSED CASHIER </h1>
+        </div>
+      
+    )
+}
+
+export default ClosedCashier;
