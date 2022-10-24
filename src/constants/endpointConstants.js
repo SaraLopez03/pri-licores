7d9894f34b9012985d2e48c73cc7da1fc52b9960
Sara Lopez <sara-lohe@hotmail.com>
2022-10-24 16:19:43 -0500

    all conditions to remove products are met01~

--- src/constants/endpointConstants.js
+++ src/constants/endpointConstants.js
@@ -3,5 +3,6 @@ export const ENDPOINT = {
     POST_LOGIN: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/user/login",
     POST_NEW_PRODUCT: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/product",
     GET_PRODUCTS: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/product",
-    PUT_UPDATE_PRODUCT: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/product"
+    PUT_UPDATE_PRODUCT: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/product",
+    DEL_DELETE_PRODUCT: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com/dev/product"
 }
