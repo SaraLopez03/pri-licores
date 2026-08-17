9486626b0710d9471e4186e1cd06cf68861875ce
Daniel Marulanda <danielmarulanda@Daniels-MacBook-Pro.local>
2026-08-16 22:55:05 -0500

    Refactor endpoint constants to support development and production environments

--- src/constants/endpointConstants.js
+++ src/constants/endpointConstants.js
@@ -1,27 +1,22 @@
 const env = process.env.NODE_ENV;
 const api_urls = {
-  // local: "http://localhost:9090",
-  local: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com",
-  prod: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com",
+  development: "http://localhost:9090",
+  production: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com",
 };
 
+const baseUrl = api_urls[env] || api_urls.development;
+
 export const ENDPOINT = {
-  POST_LOGIN: `${
-    api_urls[env] || api_urls.local || api_urls.local
-  }/dev/user/login`,
-  POST_NEW_PRODUCT: `${
-    api_urls[env] || api_urls.local || api_urls.local
-  }/dev/product`,
-  POST_SALES_BY_DATES: `${api_urls[env] || api_urls.local}/dev/sale-by-dates`,
-  GET_PRODUCTS: `${api_urls[env] || api_urls.local}/dev/product`,
-  PUT_UPDATE_PRODUCT: `${api_urls[env] || api_urls.local}/dev/product`,
-  DEL_DELETE_PRODUCT: `${api_urls[env] || api_urls.local}/dev/product`,
-  NEW_SALE: `${api_urls[env] || api_urls.local}/dev/sale`,
-  OPEN_SALES: `${api_urls[env] || api_urls.local}/dev/sales-open`,
-  UPDATE_SALE: `${api_urls[env] || api_urls.local}/dev/update-sale`,
-  POST_PAY_SALE: `${api_urls[env] || api_urls.local}/dev/pay-open-sale`,
-  GET_PENDING_SALES: `${api_urls[env] || api_urls.local}/dev/sales-pending`,
-  CLOSE_PENDING_SALES: `${
-    api_urls[env] || api_urls.local
-  }/dev/close-sales-pending`,
+  POST_LOGIN: `${baseUrl}/dev/user/login`,
+  POST_NEW_PRODUCT: `${baseUrl}/dev/product`,
+  POST_SALES_BY_DATES: `${baseUrl}/dev/sale-by-dates`,
+  GET_PRODUCTS: `${baseUrl}/dev/product`,
+  PUT_UPDATE_PRODUCT: `${baseUrl}/dev/product`,
+  DEL_DELETE_PRODUCT: `${baseUrl}/dev/product`,
+  NEW_SALE: `${baseUrl}/dev/sale`,
+  OPEN_SALES: `${baseUrl}/dev/sales-open`,
+  UPDATE_SALE: `${baseUrl}/dev/update-sale`,
+  POST_PAY_SALE: `${baseUrl}/dev/pay-open-sale`,
+  GET_PENDING_SALES: `${baseUrl}/dev/sales-pending`,
+  CLOSE_PENDING_SALES: `${baseUrl}/dev/close-sales-pending`,
 };
