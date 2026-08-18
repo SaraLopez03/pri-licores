38cec2e1fa43399b31163908c0e931aae7bb3cc1
Daniel Marulanda <danielmarulanda@Daniels-MacBook-Pro.local>
2026-08-18 16:36:26 -0500

    Point UI to prod API Gateway

--- src/constants/endpointConstants.js
+++ src/constants/endpointConstants.js
@@ -1,22 +1,22 @@
 const env = process.env.NODE_ENV;
 const api_urls = {
-  development: "http://localhost:9090",
-  production: "https://um4ajvns2b.execute-api.us-west-2.amazonaws.com",
+  development: "http://localhost:9090/dev",
+  production: "https://1gnn34v2s9.execute-api.us-west-2.amazonaws.com/prod",
 };
 
 const baseUrl = api_urls[env] || api_urls.development;
 
 export const ENDPOINT = {
-  POST_LOGIN: `${baseUrl}/dev/user/login`,
-  POST_NEW_PRODUCT: `${baseUrl}/dev/product`,
-  POST_SALES_BY_DATES: `${baseUrl}/dev/sale-by-dates`,
-  GET_PRODUCTS: `${baseUrl}/dev/product`,
-  PUT_UPDATE_PRODUCT: `${baseUrl}/dev/product`,
-  DEL_DELETE_PRODUCT: `${baseUrl}/dev/product`,
-  NEW_SALE: `${baseUrl}/dev/sale`,
-  OPEN_SALES: `${baseUrl}/dev/sales-open`,
-  UPDATE_SALE: `${baseUrl}/dev/update-sale`,
-  POST_PAY_SALE: `${baseUrl}/dev/pay-open-sale`,
-  GET_PENDING_SALES: `${baseUrl}/dev/sales-pending`,
-  CLOSE_PENDING_SALES: `${baseUrl}/dev/close-sales-pending`,
+  POST_LOGIN: `${baseUrl}/user/login`,
+  POST_NEW_PRODUCT: `${baseUrl}/product`,
+  POST_SALES_BY_DATES: `${baseUrl}/sale-by-dates`,
+  GET_PRODUCTS: `${baseUrl}/product`,
+  PUT_UPDATE_PRODUCT: `${baseUrl}/product`,
+  DEL_DELETE_PRODUCT: `${baseUrl}/product`,
+  NEW_SALE: `${baseUrl}/sale`,
+  OPEN_SALES: `${baseUrl}/sales-open`,
+  UPDATE_SALE: `${baseUrl}/update-sale`,
+  POST_PAY_SALE: `${baseUrl}/pay-open-sale`,
+  GET_PENDING_SALES: `${baseUrl}/sales-pending`,
+  CLOSE_PENDING_SALES: `${baseUrl}/close-sales-pending`,
 };
